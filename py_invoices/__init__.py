bdaf2cd1470f728b958f10256d6218cc688ac47e
Romamo <romamo@yandex.ru>
2026-01-04 16:23:55 +0200

    chore: Bump version to 1.3.1

--- py_invoices/__init__.py
+++ py_invoices/__init__.py
@@ -6,7 +6,7 @@ This package provides:
 - PDF generation capabilities
 """
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 from .config import InvoiceSettings
 from .core import AuditService, NumberingService, PDFService
