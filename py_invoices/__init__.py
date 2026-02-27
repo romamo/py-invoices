5e8526dd569c349cd71d6ebdcde96c4d4af21072
Romamo <romamo@yandex.ru>
2026-02-27 13:51:02 +0200

    chore: release v1.8.4

--- py_invoices/__init__.py
+++ py_invoices/__init__.py
@@ -6,7 +6,7 @@ This package provides:
 - PDF generation capabilities
 """
 
-__version__ = "1.8.3"
+__version__ = "1.8.4"
 
 from .config import InvoiceSettings
 from .core import AuditService, NumberingService, PDFService
